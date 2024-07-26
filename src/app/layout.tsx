7813a34f169f0254f23b60ce4ad0dda4da7d237a
raaahat <157872026+raaahat@users.noreply.github.com>
2024-07-26 16:23:13 +0600

    initial code for startup

---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import { Inter as FontSans } from 'next/font/google';
+
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
+
+export const metadata: Metadata = {
+  title: 'Template for nextjsprisma',
+  description: 'Starter code for nextjs prisma shadcnui theme project',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant="secondary"> click</Button>
+    </main>
+  );
+}
