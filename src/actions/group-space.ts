676c7b59da4b09a8c5c018df35231d134c832b7f
raaahat <157872026+raaahat@users.noreply.github.com>
2024-08-07 17:10:15 +0600

    some rename

--- src/actions/group-space.ts
+++ src/actions/group-space.ts
@@ -3,14 +3,25 @@ import { ProfileSchema } from '@/schemas';
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
 import { v4 as uuidv4 } from 'uuid';
-export const createGroupSpace = async (
-  values: z.infer<typeof ProfileSchema>
-) => {
+import { db } from '@/lib/prisma';
+export const createGroupSpace = async ({
+  name,
+  imageUrl,
+}: z.infer<typeof ProfileSchema>) => {
   try {
     const profile = await currentProfile();
     if (!profile) {
       return new NextResponse('Unauthorized', { status: 401 });
     }
+    const groupspace = await db.groupSpace.create({
+      data: {
+        ownerId: profile.id,
+        name,
+        imageUrl,
+        inviteCode: uuidv4(),
+        channels: {},
+      },
+    });
   } catch (error) {
     console.log('[SERVERS_POST]', error);
   }
